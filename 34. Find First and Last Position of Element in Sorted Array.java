0f76e5036b69b08bfd566b4cf086afe715444815
PAtrickpp33 <89336859+PAtrickpp33@users.noreply.github.com>
2022-08-03 22:25:46 +0800

    Create 34. Find First and Last Position of Element in Sorted Array.java

---
+++ 34. Find First and Last Position of Element in Sorted Array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        //find first one then find target+1
+        int l=search(nums,target);
+        int r=search(nums,target+1);
+        if(l==nums.length||nums[l]!=target)
+            return new int[]{-1,-1};
+        return new int[]{l,r-1};
+    }
+    //找>=target的第一个
+    public int search(int[] nums,int target){
+        int l=0,r=nums.length;
+        while(l<r){
+            int mid=(r+l)>>1;
+            if(nums[mid]>=target)
+                r=mid;
+            else
+                l=mid+1;
+        }
+        return l;
+    }
+}
